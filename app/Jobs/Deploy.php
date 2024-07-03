a2022fa9ae244c98850c64cf8e607a640b0b0054
Nagmeldin yousif <star@Nagmeldins-Mac-mini.local>
2024-07-03 18:13:07 +0300

    star

--- app/Jobs/Deploy.php
+++ app/Jobs/Deploy.php
@@ -4,14 +4,19 @@ namespace App\Jobs;
 
 use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class Deploy implements ShouldQueue
+// class Deploy implements ShouldQueue, ShouldBeUnique
 {
-    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use  Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
@@ -26,6 +31,50 @@ class Deploy implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        info('Started deployment ...');
+            sleep(5); // simulate deployment process
+            info('Deployment completed');
+
+
+        /** How to use Redis for conccurnt request */
+
+        // Redis:throttle for rateLimiter requests
+        // Redis::funnel('deployments')
+        // Redis::throttle('deployments')
+        // ->allow(10)
+        // ->every(60)
+        // // ->limit(5) // only 5 instance of same job can run at same time
+        // ->block(10)
+        // ->then(function() {
+        //     info('Started deployment ...');
+        //     sleep(5); // simulate deployment process
+        //     info('Deployment completed');
+        // });
+
+        /** How to use Cache for conccurnt request */
+
+        // Cache::lock('deployments')->block(10, function(){
+        //     info('Started deployment ...');
+        //     sleep(5); // simulate deployment process
+        //     info('Deployment completed');
+        // });
+
+    }
+
+    // public function unique(): string
+    // {
+    //     return 'deployments';
+    // }
+
+    // public function uniqueFor()
+    // {
+    //     return 60;
+    // }
+
+    public function middleware() {
+        return [
+            // new WithoutOverlapping('deployments', 10) // prevent run job while another instance of same job in progress
+
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,46 @@ use Illuminate\Support\Facades\Route;
 //     Bus::chain($chain)->dispatch();
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    $batch = [
-        new \App\Jobs\PullRepo('lara/project1'),
-        new \App\Jobs\RunTest('lara/project2'),
-        new \App\Jobs\Deploy('lara/project3'),
-    ];
 
-    // must add patchable trait to job class
-    Bus::batch($batch)->allowFailures()->dispatch();
-    // migrate batchble table
+
+// Route::get('/', function () {
+//     $batch = [
+//         new \App\Jobs\PullRepo('lara/project1'),
+//         new \App\Jobs\RunTest('lara/project2'),
+//         new \App\Jobs\Deploy('lara/project3'),
+//     ];
+
+//     // must add patchable trait to job class
+//     Bus::batch($batch)
+//             ->allowFailures()
+//             ->catch(function ($batch, $e) {
+//                 // $batch->cancel();
+//                 info('Batch Faild');
+//             })
+//             ->then(function ($batch) { // run after successful batch
+//                 info('Batch completed');
+//             })
+//             ->finally(function($batch) { // run after batch completed or faild
+//                 info('Batch completed or faild');
+//             })
+//             ->onQueue('deployments')
+//             ->onConnection('database')
+//             ->dispatch();
+//     // migrate batchble table
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    // Rase Condition it's a term describe when two or more jobs try
+    //to update the same record in the database at the same time
+    // setuation when tow or more processes try to make change on the same resource at the same time
+
+    \App\Jobs\Deploy::dispatch();
     return view('welcome');
 });
+
+
+// Redis Cunccurncy limiter give more flexibility we can set any limit we want
+
+
+//** How to cinfigure cuncerncy using cache or Redis above*/
